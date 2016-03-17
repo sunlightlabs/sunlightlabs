aee11ab64672fb37e54b93dffa48b007ead71c53
crdunwel <clayton.dunwell@gmail.com>
2016-03-17 03:50:49 -0400

    config stuff

--- projects/emailcongress/provision/run-this.sh
+++ projects/emailcongress/provision/run-this.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # tease out path to provisioning source from ansible cfg file
-SOURCE_PATH="$(cat ansible/ansible.cfg | grep roles_path | awk -F'=' '{print $2}' | tr -d ' ')"
+SOURCE_PATH="$(cat ansible/ansible.cfg | grep source_path | awk -F'=' '{print $2}' | tr -d ' ')"
 
 cd ansible
+ansible-galaxy install -r "$SOURCE_PATH/install_roles.yml"
 ansible-playbook provision.yml --extra-vars "source_path=$SOURCE_PATH"
 cd -
